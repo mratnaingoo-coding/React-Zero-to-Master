9465543776121a2091b7885f7e270a4b4a119cf3
Rikzil <vking854@gmail.com>
2025-07-20 21:19:16 +0630

    Second Project - Clothing Project
    Now I've done to watch video from 1 to 10 of part 4. I've already made the components of category items. Learn from this git hub below. "https://github.com/ZhangMYihua/crwn-clothing-v2"
    Thank you.

---
+++ clothing-projects/src/App.js
@@ -0,0 +1,52 @@
+import './Categories.style.scss';
+import CategoryItem from './components/Category-item-components';
+
+
+function App() {
+  const categoriesDir = [
+    {
+      id: 1,
+      title: 'Hats',
+      imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+    },
+    {
+      id: 2,
+      title: 'Jackets',
+      imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
+    },
+    {
+      id: 3,
+      title: 'Sneakers',
+      imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
+    },
+    {
+      id: 4,
+      title: 'Women',
+      imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
+    },
+    {
+      id: 5,
+      title: 'Men',
+      imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
+    },
+  ];
+
+  return (
+
+
+    <div className="categories-container">  
+      {/* Include each category */}
+      {categoriesDir.map((category) => (
+        <CategoryItem key={category.id} category={category} />
+      ))}
+    </div>
+
+
+
+
+
+
+  );
+}
+
+export default App;
